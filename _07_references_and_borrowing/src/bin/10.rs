e35dc26a0c631491d4e59bd0026a630f34c26693
David Biagiola <davidbiagiola5@gmail.com>
2025-03-16 18:46:00 -0400

    final annotations about references

--- _07_references_and_borrowing/src/bin/10.rs
+++ _07_references_and_borrowing/src/bin/10.rs
@@ -1,16 +1,21 @@
 fn main() {
+    // same example but now using tuples instead of arrays
     let registrations: (bool, bool, bool) = (true, false, true);
     let first: bool = registrations.0;
     println!("{first} and {registrations:?}");
-
+    
     let languages: (String, String) = (
         String::from("Rust"),
-        String::from("Golang")
+        String::from("Javascript")
     );
-
-    let first: String = languages.0.clone();
-
-    // let first: &String = &languages.0;
+    // same error as before so we need to use clone or reference it
+    // let first: String = languages.0;
     
-    println!("{first} and {languages:?}")
+    // duplicating the heap data
+    let first: String = languages.0.clone();
+    
+    // borrowing ownership
+    let first: &String = &languages.0;
+    
+    println!("{first} and {languages:?}");
 }
