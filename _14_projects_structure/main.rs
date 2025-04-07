91e87c1fe7c96d79ce250df6ffcf479b59815b37
David <marcelobiagiola01@gmail.com>
2025-04-07 16:50:16 -0300

    add consistency for manager const and selfnaming

--- _14_projects_structure/main.rs
+++ _14_projects_structure/main.rs
@@ -1,24 +1,28 @@
 mod inventory;
 mod orders;
 
-use inventory::products::{Item, ProductCategory};
+// use inventory::products::{Item, ProductCategory};
 // use inventory::products::Item;
-use inventory::{FLOOR_SPACE, MANAGER, talk_to_manager};
+
+// use inventory::products;
+// use inventory::products{ProductCategory};
+use inventory::products::{self, ProductCategory}; // same as two line above
+use inventory::{FLOOR_SPACE, talk_to_manager};
 
 fn main() {
     println!(
         "Our managers are {} and {}. We have {} square feet of floor space",
-        MANAGER,
+        inventory::MANAGER,
         orders::MANAGER,
         FLOOR_SPACE
     );
 
     talk_to_manager();
 
-    let favorite_category = ProductCategory::Hammer;
+    let favorite_category = products::ProductCategory::Hammer;
     println!("My fav category of item is {favorite_category:?}");
 
-    let tall_lader = Item {
+    let tall_lader = products::Item {
         name: String::from("Ladder-o-matic 2000"),
         category: favorite_category,
         quantity: 100
