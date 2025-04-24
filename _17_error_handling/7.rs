2dbc6a70789da16ebd8a833ac46fb6f0d1a62d99
David <marcelobiagiola01@gmail.com>
2025-04-24 15:16:48 -0300

    formatted code

--- _17_error_handling/7.rs
+++ _17_error_handling/7.rs
@@ -1,33 +1,30 @@
 use std::fs::File;
 use std::error::Error;
 use std::process;
-use std::io::{stdin, Read}; // import our Read trait; trait is something it'll be discuss in deep details later sections
+use std::io::{stdin, Read};
 
 fn main() {
+    // set file name to search
     println!("Please enter the name of the file you'd like to read:");
     let mut input: String = String::new();
     
     let user_requested_file = stdin().read_line(&mut input);
-    
-    // we're declaring a error variable and a block code conditionally
-    if let Err(error) = user_requested_file { // remember the Err variant is also available from the rust prelude, the same will be to put it like this: Result::Err
+    if let Err(error) = user_requested_file {
         eprintln!("Something went wrong colleting user input. The error was {error}");
         process::exit(1)
     }
-    
-    // continues if there's no error
-    // this mut file variable is more for the internal values of reading a file
-    // that will change to keeping track wheather the file has been read etc.
-    let mut file = match File::open(&input.trim()) { // also, read_line captures the break line as one more character too, example story.txt/n
+
+    // open the file
+    let mut file = match File::open(&input.trim()) {
         Ok(file) => file,
         Err(error) => {
             eprintln!("Something went wrong opening the file. The error was {error:?}");
-            process::exit(1); // <- Exit the program so Rust knows we won't continue
+            process::exit(1)
         }
     };
 
+    // read its content
     let mut file_content: String = String::new();
-    // let read_operation: Result<usize, Error> = file.read_to_string(&mut file_content);
     let read_operation = file.read_to_string(&mut file_content);
 
     if let Err(error)  = read_operation {
@@ -38,3 +35,12 @@ fn main() {
     println!("{:#?}", file_content)
     
 }
+
+// Side notes
+// 1. About Read trait import, is a kind of trait we'll discuss more in later sections.
+// 2. When we declaring let if statements, we're declaring a error variable and a block code conditionally.
+// 3. Remember the Err variant is also available from the rust prelude, the same will be to put it like this: Result::Err
+// 4. The mut file variable is more for the internal values of reading a file that will change to keeping track whether the file has been read or not.
+// also, read_line captures the break line as one more character too, example story.txt/n
+// ----------------
+// 5 . let read_operation: Result<usize, Error> = file.read_to_string(&mut file_content);
