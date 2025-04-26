85cc2181dbe36ba159d5ced13ca6abb16b43d70e
David Biagiola <marcelobiagiola01@gmail.com>
2025-04-26 15:44:33 -0300

    add some notes about tuples and structs similarities

--- _04_functions/src/bin/02.rs
+++ _04_functions/src/bin/02.rs
@@ -1,8 +1,17 @@
 fn main() {
 	// example of a normal tuple
-	let result: (i32, bool, &str) = (5, true, "hello");
+	let student: (i32, bool, &str) = (99, true, "David Biagiola");
+	println!("{student:?}");
 
-	// example of a unit
+	// also we can extract value from the tuple
+	let (age, is_active, name) = student;
+	println!("{}, is studying right now?: {}", name, is_active);
+
+	// or access those value by index
+	let (age, is_active, name) = student;
+	println!("{}, is studying right now?: {}", student.2, student.1);
+
+	// example of a unit, most simple tuple
 	let emptyTuple: () = ();
 
 	// an empty function return a unit
@@ -12,3 +21,8 @@ fn main() {
 fn mystery() {
 	println!("Hello"); // mystery also can make operations
 }
+
+
+// the student tuple could be an struct, and structs are more heavy meanwhile tuples
+// are focus for quick and lightwieght data. With strcuts we can achieve more comples
+// goals and also OOP.
