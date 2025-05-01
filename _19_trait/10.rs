ef9a02df9e6523d7725872cbef2a1e61250bf095
David Biagiola <marcelobiagiola01@gmail.com>
2025-05-01 11:21:12 -0300

    example to cast string numbers to real numbers

--- _19_trait/10.rs
+++ _19_trait/10.rs
@@ -3,9 +3,14 @@
 // lasted lectures we defined our traits in the same file but now
 // in our case we need to imported because its definition is somewhere else
 use std::ops::Add; // be brint the Add trait from the standard operation module
+use std::str::FromStr;
+
 fn main() {
     let a: i32 = 5;
     let b: i32 = 10;
     let sum = a.add(b);
     println!("{sum}");
+
+    let numeric_count = u64::from_str("5");
+    println!("{}", numeric_count.unwrap());
 }
