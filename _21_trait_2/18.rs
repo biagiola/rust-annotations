d73cb9dbd4afbca0e090bedb5d0efd65f03fdb6f
David Biagiola <marcelobiagiola01@gmail.com>
2025-05-13 21:33:45 -0300

    add example of Associated types for traits

---
+++ _21_trait_2/18.rs
@@ -0,0 +1,33 @@
+// Lecture: Associated types.
+// this is a placeholder for a type that is required within a trait
+// similar to generics but specific for trait definitions
+
+use std::ops::Add; // we import the add trait
+
+#[derive(Debug)]
+struct Lunch {
+    cost: f64
+}
+
+impl Add for Lunch {
+    type Output = f64; // this name is something that is defined in the trait
+                       // and we need to implement it 
+
+    fn add(self, rhs: Self) -> Self::Output {
+        self.cost + rhs.cost + 2.0
+    }
+}
+
+fn main() {
+    let one: Lunch = Lunch { cost: 19.99 };
+    let two: Lunch = Lunch { cost: 29.99 };
+    println!("{:.2}", one + two);
+
+    // now, when rust see two structs and the plus sign
+    // he knows that method to implement, the Add method from
+    // the Lunch struct. Add is already imported from the operations
+    // trait, so when we impl the Add method for the Lunch
+    // we must to honor that trait, but of course we can make our
+    // our logic operation there. For example for some kind of rules
+    // our sums is a + b plus an extra fees o whatever. 
+}
