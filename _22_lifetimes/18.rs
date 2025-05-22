3c4ff0db1a473019c4af9c90cf625b2f3f25a8f0
David <marcelobiagiola01@gmail.com>
2025-05-22 09:17:47 -0300

    corrections in the explanation of multiple lifetime params on error situation

--- _22_lifetimes/18.rs
+++ _22_lifetimes/18.rs
@@ -1,33 +1,34 @@
-// Lecture: Multiple parameters II
-// solution for the previous lesson
+// Lecture: Multiple parameters part II
+// Solution for the previous lesson
 
-// In this way we're saying we're considering the lifetime of first param.
-// In this case we don't need the 'b but we use it just for now.
-// Now we're saying that both parameters has different independent lifetimes.
+// In this way we're saying we're considering the lifetime of the first param.
+// In this case, we don't need the 'b, but we use it just for demonstration purposes.
+// Now we're saying that both parameters have different, independent lifetimes.
 //
 fn longest<'a, 'b>(first: &'a str, second: &'b str) -> &'a str {
-    // So coz we're return just first we cannot longer use this code,
-    // remember our goal here now, is to make our main function compile
-    // and understand about multiple lifetimes.
+    // So, because we're returning just `first`, we can no longer use this code
+    // exactly how we used it in the previous example.
     // if first.len() > second.len() {
     //     first
     // } else {
     //     second
     // }
+    // Remember, our goal now is to make our main function compile and understand multiple lifetimes.
+    
+    println!("{second}"); // Lifetime doesn't affect the params at runtime, so we can keep using it,
+    first // but now we're telling with 'a that we should return `first`
 
-    println!("{second}"); // this is just to show we can use the param of course
-    first // but now we telling with 'a we should return first
+    // We're saying to the borrow checker now that 'b doesn't matter for the
+    // sake of persistence. All Rust needs to guarantee is that the referent—
+    // the `result` variable, where the param `first` is coming from—is where the return
+    // reference (`first`) is going to live within.
+    // So the borrow checker understands that `result` will be a reference to, effectively,
+    // `orlando` and not `san_francisco`. That's all said in the function `longest`'s
+    // signature, and the borrow checker knows it.
 
-    // we're saying to the borrow checker now that the 'b doesn't matter for the 
-    // sake of persisence. All the rust needs to guarantee is that the referent,
-    // result, that the param 'first' is coming from, is where the return reference
-    // is going to live within. So the borrow checker understands 'results' will
-    // be a reference to effectively 'orlando' and not 'san_francisco'. That's all
-    // said it in the function longest signature and the borrow checker knows it.
-
-    // So san_francisco is going to go out of its scope, its lifetimes ends but 
-    // there's no way we can have a dangling refernce to that thanks to our 
-    // correct generic lifetimes annotations.
+    // So, `san_francisco` is going to go out of its scope—its lifetime ends—but
+    // there's no way we can have a dangling reference to that, thanks to our
+    // correct generic lifetime annotations.
 }
 
 fn main() {
@@ -36,7 +37,5 @@ fn main() {
     let result = {
         let san_francisco = String::from("San Francisco");
         longest(&orlando, &san_francisco)
-    }
-
+    };
 }
-
