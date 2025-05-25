91913a9a857ce2568c34da2218814eaa4e592998
David Biagiola <marcelobiagiola01@gmail.com>
2025-05-25 17:15:11 -0300

    multiple lifetime references on struct using other functions

--- _22_lifetimes/24.rs
+++ _22_lifetimes/24.rs
@@ -1,15 +1,12 @@
 // Lecture: Multiple lifetimes in structs
+// same example as before but now using other functions
+// instead of inner scope blocks. Apply the same rules
+// we're talked about.
+// Remember that the end goal of the borrow checker is
+// to make sure that we avoid dangling references and
+// especially as you start passing in references in and
+// out of functions.
 
-// So the key difference here is that rust is able to figure
-// out that the 'from' lifetime and the 'to' lifetime are 
-// different, and it doesn't have to pick one to play the role
-// of the single lifetime that we saw in the previous example.
-// Rust understand now, that the 'from' lifetime will be the concrete
-// lifetime for generic lifetime a, and the 'to' lifetime will be the
-// concrete lifetime for generic b. And since it understands they're
-// different and its associations of the fields, it know that if we
-// return 'from', it know that from is the connected to a lifetime,
-// thus it doesn't represent a dangling reference.
 #[derive(Debug)]
 struct TravelPlan<'a, 'b> {
     from: &'a str,
@@ -18,17 +15,18 @@ struct TravelPlan<'a, 'b> {
 
 fn main() {
     let from = String::from("Portland");
-
-    let plan: &str = {
-        let to = String::from("Bangor");
-
-        let travel_plan: TravelPlan<'_, '_> = TravelPlan {
-            from: &from,
-            to: &to,
-        };
-
-        travel_plan.from
-    };
+    let plan: &str = figure_out_ending_point(&from);
     
     println!("{plan}");
+}
+
+fn figure_out_ending_point(from: & str) -> &str {
+    let to = String::from("Bangor");
+
+    let travel_plan: TravelPlan<'_, '_> = TravelPlan {
+        from: &from,
+        to: &to,
+    };
+
+    travel_plan.from
 }
