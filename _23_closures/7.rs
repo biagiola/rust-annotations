d98f71dda4d3559c3671f1d2979efe559a4726d0
David Biagiola <marcelobiagiola01@gmail.com>
2025-06-06 21:27:20 -0300

    add example of move keyword that overrides default rust behaviour on closures

---
+++ _23_closures/7.rs
@@ -0,0 +1,39 @@
+// Lecture: The move keyword
+// Rust has a special keyword called move that will force
+// the movement of ownership of values into a closure even
+// when the closure won't do so by default.
+// There are situations where this move keyword is required
+// particularly when dealing with multithreaded programs.
+
+fn main() {
+    let first_name = String::from("Alice");
+    let last_name = String::from("Wonder");
+
+    // let capture_string = || {
+    //     // since we're just reading, we're not borrowing anything.
+    //     println!("{first_name}");
+    // };
+
+    let capture_string = move || {
+        // but with the move keyword we can force to take ownership
+        // so forcde the borrow of immutable references of rust.
+        println!("{first_name} {last_name}");
+    };
+    // println!("{first_name}"); // so, both values was moved
+
+    // the big confution is that if we see the type that rust analyzer
+    // give us is just a Fn closure (impl Fn()) and we can indeed
+    // invoke it multiple times.
+    capture_string();
+    capture_string();
+    capture_string();
+    // So, it's ultimately how the moved values are used in the closure,
+    // after the movement of ownership that determines how the compiler
+    // will infer the type of the closure and which one of those three
+    // FN traits it chooses.
+
+    // we can wonder why is a FN and why is it capable of running multiple times?
+    // Why is it not an FnOnce
+    // We can find more discussion about this on: https://www.udemy.com/course/learn-to-code-with-rust/learn/lecture/47564919#overview
+}
+
