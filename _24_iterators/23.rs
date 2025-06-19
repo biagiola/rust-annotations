75dc78aacc73de57bb11b572966a01695ff56811
David Biagiola <marcelobiagiola01@gmail.com>
2025-06-19 20:37:43 -0300

    example using find method

---
+++ _24_iterators/23.rs
@@ -0,0 +1,23 @@
+// Lecture: The filter and find methods IV
+
+fn main() {
+    let numbers = [10, 13, 23, 2, 8, 9, 6];
+
+    // let first_even: Option<i32> = numbers.into_iter().find(|number| number % 2 == 0);
+    // let first_even: Option<&i32> = numbers.into_iter().find(|number| *number % 2 == 0);
+    // let first_even: Option<i32> = numbers.into_iter().copied().find(|number| number % 2 == 0);
+    let first_even: Option<i32> = numbers.iter().copied().find(|number| number % 2 == 0);
+    println!("{first_even:?}");
+
+    let first_odd: Option<i32> = numbers.iter().copied().find(|number| number % 2 != 0);
+    println!("{first_odd:?}");
+
+    let nothing: Option<i32> = numbers.iter().copied().find(|number| *number > 100);
+    println!("{nothing:?}");
+
+    let last_even: Option<i32> = numbers.iter().copied().rfind(|number| number % 2 == 0);
+    println!("{last_even:?}");
+
+    let last_odd: Option<i32> = numbers.iter().copied().rfind(|number| number % 2 != 0);
+    println!("{last_odd:?}");
+}
