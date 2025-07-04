2e96bdb220ee4bfe5a3b851798ba63d4244060ac
David <marcelobiagiola01@gmail.com>
2025-07-04 12:22:35 -0400

    more clarity on str references

--- _06_ownership/12.rs
+++ _06_ownership/12.rs
@@ -1,16 +1,15 @@
-
 // String: A dynamically allocated, growable mutable piece of text stored on the heap.
 // &String ("ref String"): A reference to a String, typically used for borrowing.
 // str: A string slice (not commonly used by itself in Rust code).
 // &str ("ref str"): A reference to a string slice, typically used to reference hardcoded text (string literals) or substrings.
 
 fn main() {
-    let ice_cream: &str = "Cookies and Crem";
-    // this reference points neither the stack or the heap
-    // it's reference to the hardcoded text that was embedded into
-    // the executable file that is then loaded into the memory.
+    let ice_cream: &str = "Cookies and Cream";
+    // this reference points to neither the stack nor the heap
+    // it's a reference to the hardcoded text that was embedded into
+    // the executable file and is loaded into the program's data segment in memory.
 
-    println!("{}", ice_cream)
+    println!("{}", ice_cream);
 
-    // Try the extension on VSCode: Hex Editor for see the binaries
+    // Try the extension on VSCode: Hex Editor to see the binaries
 }
--- _06_ownership/13.rs
+++ _06_ownership/13.rs
@@ -2,11 +2,13 @@ fn main() {
     let ice_cream: &str = "Cookies and Cream";
     let dessert: &str = ice_cream;
     
-    // we're having two reference in our example, the reference of 'dessert'
+    // we have two references in our example, the reference of 'dessert'
     // is a full copy of the reference from 'ice_cream', so now
-    // both references the same "Cookies and Cream" text from the actual
+    // both reference the same "Cookies and Cream" text from the actual
     // binary.
 
     println!("{:p}", ice_cream);
     println!("{:p}", dessert);
+    // Both will print the same address since they point to the same location
+    // in the program's data segment
 }
