8e65ffa7086e2286afa2d31e401d7578dc3fc1c6
David <marcelobiagiola01@gmail.com>
2025-07-04 12:52:56 -0400

    fix on comment about copy trait of a stack value

--- _06_ownership/14.rs
+++ _06_ownership/14.rs
@@ -1,18 +1,18 @@
 fn main() {
     let apples: i32 = 6; // a stack variable
     
-    // stack variables are primitives, does not have
+    // stack variables are primitives, do not have
     // a pointer representation like heap-allocated data
-    // so, we use & for print its reference
-    // println!("{:p}", &apples);
+    // so, we use & to print its reference
+    println!("{:p}", &apples);
     print_my_value(apples);
     println!("{apples} is still valid");
 }
 
 fn print_my_value(value: i32) {
-    // println!("{:p}", &value);
+    println!("{:p}", &value);
 
     println!("Your value is {value}");
-    // here, we implement copy trait. Value is a new reference
-    // and it's deallocate after finish its scope
+    // here, the Copy trait is automatically used. 'value' is a new copy
+    // of the original data, and it gets deallocated when this scope ends
 }
