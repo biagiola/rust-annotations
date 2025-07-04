6c24b50c4b8e36a01c36aed5c60b05413476f6d4
David <marcelobiagiola01@gmail.com>
2025-07-04 11:49:57 -0400

    explanation on references and pointers

--- _06_ownership/10.rs
+++ _06_ownership/10.rs
@@ -1,20 +1,19 @@
 // Borrowing means using something without taking ownership of it.
-// Most of the time, we're going to be creating references to heap data
-// rather from stack, that is also possible but cheaper to worry about.
+// Most of the time, we create references to heap data rather than stack data.
+// Referencing stack data is possible but often unnecessary due to memory costs.
 
 fn main() {
-    // example of borrowring data from the stack
+    // example of borrowing data from the stack
     let my_stack_variable: i32 = 22;
-    let my_integer_referece: &i32 = &my_stack_variable;
-    // making a reference of a stack value could be the same
-    // or ever more than memory required for a integer, that's
+    let my_integer_reference: &i32 = &my_stack_variable;
+    // making a reference of a stack value could use the same
+    // or even more memory than the integer itself, that's
     // why we don't usually use references for stack variables
 
     let my_heap_value: String = String::from("Toyota");
     let my_heap_reference: &String = &my_heap_value;
 
-    // References and pointers is almost the same.
-    // Reference is a subcategory of pointer.
-    // In Rust, a reference is a valid value of a pointer.
-    // References must never outlive the referent (the value).
+    // References are a type of safe pointer in Rust.
+    // Unlike raw pointers, references are always valid and safe to use.
+    // References must never outlive the referent (the value they point to).
 }
