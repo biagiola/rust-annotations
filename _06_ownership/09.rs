e8122f5840705f0de9f79a95e64f909a598972a6
David <marcelobiagiola01@gmail.com>
2025-07-03 23:41:40 -0400

    clone method comments

--- _06_ownership/09.rs
+++ _06_ownership/09.rs
@@ -2,18 +2,19 @@ fn main() {
     let person: String = String::from("David");
 
     // we can clone it to have both values on the heap
+    // Note: clone() has a performance cost - it allocates new heap memory and copies the data
     let user: String = person.clone();
 
     // values
     println!("{person}");
     println!("{user}");
 
-    // references
+    // references - these show the stack addresses of the variables themselves
     println!("{:p}", &person);
     println!("{:p}", &user);
     
-    // with the & we have its reference (memory address)
-    // but for print it, we need to use the format specifier
-    // for pointer {:p}, otherwise, it will print the value
-    // at the address memory.
+    // with the & we get the stack address where each variable is stored
+    // {:p} format specifier prints these stack addresses as pointers
+    // Note: this shows where the String structs are on the stack,
+    // not where the actual string data is stored on the heap
 }
