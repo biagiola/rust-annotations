43eafbafa16fb0ad86e47c8df30e67ca0ba53de9
David <marcelobiagiola01@gmail.com>
2025-07-06 13:15:19 -0400

    correction on having multiple kind of references (immutable, mutable) at the same time

--- _07_references_and_borrowing/2.rs
+++ _07_references_and_borrowing/2.rs
@@ -2,23 +2,24 @@ fn main() {
     let mut car: String = String::from("Red"); // 'car' is a mutable String
 
     let ref1: &mut String = &mut car;          // `ref1` is a mutable reference to `car`
-
     ref1.push_str(" and Blue");
 
     // create a new mutable reference
-    // coming for the car again
+    // coming for the car again. ref2 will have
+    // the previous modifications of ref1.
     let ref2: &mut String = &mut car;
-    
-    // print the v alues
+    ref2.push_str("!!!!");
+
+    // print the values
     println!("{ref2}");
-    println!("{car}"); // still exists, free to be altered by ref2 of course.
+    println!("{car}"); // still exists
     
-    // println!("{ref1}"); // but ref1 does not exists anymore
-    // Now ref2 is taking the place of ref1.
+    // but ref1 does not exists anymore, now ref2 is taking the place of ref1.
     // ref2 is immutable, so the mutable rule of ref1 and the value itsel is 'dropped'.
     // lifetime of ref1 is ended after ref2 mutable declaration and assignation.
 
-    // In Rust, you cannot have mutable and immutable reference to the same variable.
+    // In Rust, you cannot have multiple mutable references OR a mix of mutable and
+    // immutable references at the same time.
 }
 
 // this is an advance feature in rust called lifetimes.
@@ -32,4 +33,4 @@ fn main() {
 
 // Behind the scenes there is a line after the 'ref1.push_str(" and Blue");'
 // Drop the mutable reference ref1 before creating ref2
-drop(ref1);
+// drop(ref1);
