f384b4f3445deac6a6da7f06b3c6b22cfe1e1a02
David <marcelobiagiola01@gmail.com>
2025-07-07 19:29:52 -0400

    delete unused variable

--- _08_slices/5.rs
+++ _08_slices/5.rs
@@ -2,8 +2,7 @@ fn main() {
     let mut action_hero: String = String::from("Arnold Schwarzenegger");
 
     let ref1: &String = &action_hero; // Immutable borrow
-    let ref2: &mut String = &mut action_hero; // ❌ ERROR: cannot borrow as mutable because it’s already borrowed as immutable
+    // let ref2: &mut String = &mut action_hero; // ❌ ERROR: cannot borrow as mutable because it’s already borrowed as immutable
 
     println!("{ref1}");
-    ref2.push_str(" is the Terminator");
 }
