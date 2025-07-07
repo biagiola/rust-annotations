11125135989bc30b7c00e3dac4e49269ae1030a2
David <marcelobiagiola01@gmail.com>
2025-07-07 19:42:10 -0400

    more info about static lifetime information

--- _08_slices/7.rs
+++ _08_slices/7.rs
@@ -2,18 +2,17 @@ fn main() {
     let first_name: &str = {
         let action_hero: &str = "Arnold Schwarzenegger";
         &action_hero[0..6]
-    }
+    };
 
-    // action_hero is deleted after finish its block scope
-    // and first_name is still valid.
-    // the chunck of information about the aciton_hero
-    // is still available and first_name is using that
-    // but just a portion. In other words "Arnold Schwarzenegger"
-    // string is embedded in memory from the compiled file.
+    // The variable action_hero goes out of scope after finishes its block scope,
+    // but the string literal data "Arnold Schwarzenegger" has 'static lifetime and remains in memory.
+    // Variable first_name still points to valid static data.
+    // The chunk of information about the action_hero is still available and first_name
+    // is using that but just a portion.
+    // In other words "Arnold Schwarzenegger" string is embedded in memory from the compiled file.
     println!("{first_name}");
 
-    // Everytime we're declaring a string literal
-    // we're declaring a slice.
+    // Every time we're declaring a string literal we're declaring a slice.
 
     // 1. String literals are stored directly in the binary (read-only memory)
     // 2. They are references (&str), pointing to a fixed location
