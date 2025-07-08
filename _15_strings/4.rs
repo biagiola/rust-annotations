7d303bc616ae97492fed806ee83c8c2307a2edb4
David <marcelobiagiola01@gmail.com>
2025-07-08 19:40:01 -0400

    format! macro can be used with string and &str also

--- _15_strings/4.rs
+++ _15_strings/4.rs
@@ -5,9 +5,14 @@ fn main() {
     let last_name: String = String::from("Stallone");
 
     // let icon: String = format!("{} {}", first_name, last_name); 
-    let icon: String = format!("{first_name} {last_name}"); // we're concatenating the two strings through borrowing
+    // You can use both String and &str with format!
+    let icon: String = format!("{first_name} {last_name}"); // using String variables
+    let icon2: String = format!("{} {}", "Sylvester", "Stallone"); // using &str literals
+    let icon3: String = format!("{} {}", &first_name, &last_name); // using &String (which derefs to &str)
     // print our new heap allocated string
     println!("{icon}");
+    println!("{icon2}");
+    println!("{icon3}");
 
     // they don't lose ownership
     println!("{first_name}");
