cc25e9a9fb7fd05976f2691b5c1ce6ac86e981ad
David <marcelobiagiola01@gmail.com>
2025-07-08 19:34:26 -0400

    modify comments and about result of reading user input

--- _15_strings/6.rs
+++ _15_strings/6.rs
@@ -2,15 +2,16 @@ use std::io;
 
 fn main() {
     let mut name = String::new();
-    println!("What it's your name?");
+    println!("What's your name?");
     
     // io::stdin().read_line(&mut name).expect("Failed to collect input from the user");
-    // this throws us an Result with Ok or Err, if it's okay
-    // it will return the number of bytes of the input.
+    // This returns a Result<usize, Error>. If Ok, it contains the number of bytes read.
+    // If Err, it contains the error details.
     match io::stdin().read_line(&mut name) {
-        Ok(_) => println!("Hello, {}.", name.trim()), // in the _ variable we have the number of types of the user's input. And also remove the print line enter user using the trim.
-        Err(message) => println!("There was an error: {}", message)
+        Ok(bytes_read) => {
+            println!("Hello, {}!", name.trim()); // Remove the newline character (enter) pressed by the user
+            println!("(Read {} bytes of input)", bytes_read);
+        },
+        Err(error) => println!("There was an error reading input: {}", error)
     }
-    
-    
 }
