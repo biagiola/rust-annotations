8d7e7222ab3ceedcfbc35f73d24d200bb28e0443
David <marcelobiagiola01@gmail.com>
2025-07-08 09:48:31 -0400

    add comments for modify variables outside on functions

--- _08_slices/13.rs
+++ _08_slices/13.rs
@@ -1,7 +1,9 @@
-fn add_last_name(mut hero_name: String) -> String {
+// Ownership Transfer Pattern
+fn add_last_name(mut hero_name: String) -> String { // Takes ownership
     hero_name.push_str(" Schwarzenegger");
-    hero_name
+    hero_name // Returns owned String
 }
+
 fn main() {
     let action_hero: String = String::from("Arnold");
     let new_name: String = add_last_name(action_hero);
--- _08_slices/14.rs
+++ _08_slices/14.rs
@@ -1,6 +1,8 @@
-fn add_last_name(hero_name: &mut String) {
-    hero_name.push_str(" Schwarzenegger")
+// Mutable Reference Pattern
+fn add_last_name(hero_name: &mut String) { //Borrows mutably
+    hero_name.push_str(" Schwarzenegger")  // Modifies in place
 }
+
 fn main() {
     let mut action_hero: String = String::from("Arnold");
     add_last_name(&mut action_hero);
