62706c52c65e086bf0c3781107c188e516f96239
David <marcelobiagiola01@gmail.com>
2025-07-08 09:08:51 -0400

    better explanation on deref coercion

--- _08_slices/12.rs
+++ _08_slices/12.rs
@@ -10,10 +10,8 @@ fn main() {
     do_hero_stuff(another_action_hero);
     // the fn will not work if there's a mismatch between
     // ref str and ref String (&str and &String).
-    // ref str it is more versatail and rust is complete flexible enough
-    // to treat String as &str with the deref coercion. Coz...
-    // a ref String (&String) can represented as a slice of the whole with
-    // ref str (&str)
+    // "&str is more versatile because Rust automatically converts &String to &str
+    // through deref coercion, making functions more flexible."
     // &String -> &str - is okay, a slice could be the whole string
     // &str -> &String - a slice could not always be the whole &String
 }
