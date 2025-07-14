0a31ee22216f4369ee0435a31df7e647f373ad90
David <marcelobiagiola01@gmail.com>
2025-07-14 16:49:07 -0400

    fix: add mutability to song_1 modify by double_length function

--- _09_structs/17.rs
+++ _09_structs/17.rs
@@ -1,3 +1,6 @@
+// This example demonstrates methods that borrow the struct instance,
+// either mutably (`&mut self`) or immutably (`&self`).
+
 #[derive(Debug)]
 
 struct BobDylanSong {
@@ -7,24 +10,29 @@ struct BobDylanSong {
 }
 
 impl BobDylanSong {
+    // This method takes a mutable reference, allowing it to modify the instance.
+    // The `&mut self` syntax is shorthand for `self: &mut Self`.
     fn double_length(&mut self) {
-        self.duration_secs * 2;
+        self.duration_secs *= 2; // multiply and reassigning the new value
     }
 
+    // This method takes an immutable reference, so it can only read data.
+    // The `&self` syntax is shorthand for `self: &Self`.
     fn is_longer_than(&self, other: &Self) -> bool {
         self.duration_secs > other.duration_secs
     }
 }
 
 fn main() {
-    let song_1: BobDylanSong = BobDylanSong {
+    // we're going to modify one field so we need to make as mut
+    let mut song_1: BobDylanSong = BobDylanSong {
         title: String::from("Blowing in the wind"),
         release_year: 1964,
         duration_secs: 210
     };
 
     let song_2: BobDylanSong = BobDylanSong {
-        title: String::from("Like a rolling stones"),
+        title: String::from("Like a Rolling Stone"),
         release_year: 1965,
         duration_secs: 194
     };
@@ -34,4 +42,9 @@ fn main() {
     } else {
         println!("{} is longer than {}", song_2.title, song_1.title);
     }
+
+    // We can call `double_length` because `song_1` is mutable.
+    println!("Original duration of '{}': {}", song_1.title, song_1.duration_secs);
+    song_1.double_length();
+    println!("New duration after doubling: {}", song_1.duration_secs);
 }
