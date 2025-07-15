b87d8431f835b817d6709cd15cff0f422898341a
David <marcelobiagiola01@gmail.com>
2025-07-15 15:42:18 -0400

    example in detail on let else block construct

--- _10_enums/13.rs
+++ _10_enums/13.rs
@@ -1,35 +1,33 @@
-// The let else construct, introduced with rust 1.6.5
+// The `let-else` construct, introduced with rust 1.6.5 allows for "refutable" pattern matching.
+// It's a concise way to ensure a variable matches a certain pattern
+// and to diverge (e.g., return) if it doesn't.
+
 enum Milk {
     Lowfat(i32),
     Whole,
-    NonDairy{ kind: String }
+    NonDairy { kind: String },
+}
+
+fn handle_milk(my_beverage: Milk) {
+    // We attempt to match the `Milk::Lowfat` pattern against `my_beverage`.
+    let Milk::Lowfat(percent) = my_beverage else {
+        // If the pattern does NOT match, this block is executed.
+        println!("The beverage is not lowfat milk.");
+        // We must diverge (e.g., return) from the function here.
+        return;
+    };
+
+    // This code is only reachable IF the pattern matched successfully.
+    // The `percent` variable was created and is now available.
+    println!("Success! You have {percent}% lowfat milk.");
 }
 
 fn main() {
-    // let's suppose my_beverage is dynamic. Enum example
+    println!("--- Testing Lowfat Milk ---");
     let my_beverage: Milk = Milk::Lowfat(2);
+    handle_milk(my_beverage);
+
+    println!("\n--- Testing Whole Milk ---");
     let my_beverage: Milk = Milk::Whole;
-
-    let Milk::Lowfat(percent) = my_beverage else {
-        // if compared variables are not equal... enter this else block
-        println!("You don't have the lowfat milk");
-        
-        // and make sure to finish here
-        return
-    };
-
-    // percent is only declare when compared variables are not equals
-    // so in that case we don't enter the else scope
-    println!("{percent}% is available here");
-
-    // example with structs
-    let my_beverage: Milk = Milk::NonDairy {
-        kind: String::from("Oat")
-    };
-
-    let Milk::NonDairy { kind } = my_beverage else {
-        println!("You do not have the nondairy milk");
-        return
-    };
-    println!("{kind} milk is available here");
+    handle_milk(my_beverage);
 }
