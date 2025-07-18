9fb311011f416406a8ec432d46141d07c8089cd3
David <marcelobiagiola01@gmail.com>
2025-07-18 09:25:09 -0400

    box smart pointer solution for our indirection when we dealing with function calls

---
+++ _26_smart_pointers/19.rs
@@ -0,0 +1,27 @@
+#[derive(Debug)]
+enum LinkedListUsingBox<T> {
+    Empty,
+    Node {
+        value: T,
+        next: Box<LinkedListUsingBox<T>>
+    }
+}
+
+fn create_list() -> LinkedListUsingBox<i32> {
+    let second_node = LinkedListUsingBox::Node {
+        value: 2,
+        next: Box::new(LinkedListUsingBox::Empty),
+    };
+
+    let first_node = LinkedListUsingBox::Node {
+        value: 1,
+        next: Box::new(second_node), // here next is the new owner and we dont care about internal variable existance
+    };
+
+    first_node
+}
+
+fn main() {
+    let list = create_list();
+    println!("{list:#?}");
+}
