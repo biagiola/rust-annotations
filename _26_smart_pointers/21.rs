0c9e82bd24a24e1ec914ebe4295750d0435489c3
David <marcelobiagiola01@gmail.com>
2025-07-18 16:23:07 -0400

    add binary tree example

---
+++ _26_smart_pointers/21.rs
@@ -0,0 +1,51 @@
+use std::cmp::Ordering;
+
+#[derive(Debug)]
+enum BinarySearchTree {
+    Empty,
+    Node {
+        value: i32,
+        left: Box<BinarySearchTree>,
+        right: Box<BinarySearchTree>,
+    },
+}
+
+impl BinarySearchTree {
+    fn new() -> Self {
+        BinarySearchTree::Empty
+    }
+
+    fn insert(&mut self, new_value: i32) {
+        match self {
+            BinarySearchTree::Empty => {
+                // * does not mean the address like &, instead the access to the
+                // actual value on that address in memory. So, we are saying that the value
+                // in that memory will be our values of the node we wanna set
+                *self = BinarySearchTree::Node {
+                    value: new_value,
+                    left: Box::new(BinarySearchTree::Empty),
+                    right: Box::new(BinarySearchTree::Empty),
+                }
+            },
+            BinarySearchTree::Node { value, left, right } => {
+                match new_value.cmp(value) {
+                    Ordering::Equal => (), // we ignore duplicates, sake of simplicity.
+                    Ordering::Less => left.insert(new_value),
+                    Ordering::Greater => right.insert(new_value),
+                }
+            },
+        }
+    }
+}
+
+fn main() {
+    let mut tree = BinarySearchTree::new();
+    tree.insert(5);
+    tree.insert(2);
+    tree.insert(4);
+    tree.insert(8);
+    tree.insert(13);
+    tree.insert(7);
+
+    println!("{tree:#?}");
+}
