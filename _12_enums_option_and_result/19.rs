a23ebf7d5a6897312ef5e24f59517ae2edc5e624
David <marcelobiagiola01@gmail.com>
2025-07-21 11:21:52 -0400

    delete old folder

--- _12_enums_option_and_result/19.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let present_value: Option<i32> = Some(13);
-    let missing_value: Option<i32> = None;
-
-    // fallback error value to avoid runtime error
-    println!("{}", present_value.unwrap_or(0));
-    println!("{}", missing_value.unwrap_or(0)); // it must be the same type
-}
