2fe68ddd1ee85d8ac964ad1879c46674ef6d86f8
David <marcelobiagiola01@gmail.com>
2025-07-21 10:18:27 -0400

    add more difference details between unwrap and expect methods

--- _12_enums_option_and_result/13.rs
+++ _12_enums_option_and_result/13.rs
@@ -13,3 +13,14 @@ fn main() {
     // in the case where we don't have the vlaue to extract, we emit a message
     println!("{}", valid_instrument);
 }
+
+/*
+unwrap()
+What it does: If the value is Some(v) or Ok(v), it gives you v. If it's None or Err, it panics with a generic, default error message.
+When to use it: Mostly in prototypes, quick tests, or examples where you are absolutely certain the operation won't fail. It's simple to type, but its generic panic message can make debugging harder.
+
+expect()
+What it does: Exactly the same as unwrap(), but if it panics, it uses a custom message that you provide.
+When to use it: In production-ready code. When the program panics, the custom message tells you exactly which assumption was violated and why you expected a value to be there. This is incredibly helpful for debugging.
+
+*/
