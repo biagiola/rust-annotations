23b5e2712ed16a11fd14227e4d6151eaf1f7b45a
David <marcelobiagiola01@gmail.com>
2025-07-23 16:06:54 -0400

    add comment about trait bound and coherence rules

--- _19_trait/7.rs
+++ _19_trait/7.rs
@@ -1,8 +1,14 @@
-// Lecture: Traits Bound to conditionally implements methods
+// Lecture: Traits Bound and conditionally implements methods
+
+// now,  in the Hotel struct , the name field will not be a String anymore
+// String implements Display trait for println! and also for format!, but now
+// we're using generics, and we are not sure about if the incomming value wil
+// implements the Display trait so we need to handle that, specially in the 
+// summary method.
 use std::collections::HashMap;
 use std::fmt::Display;
 
-trait Accommadation {
+trait Accommodation {
     fn book(&mut self, name: &str, nights: u32);
 }
 
@@ -18,7 +24,7 @@ struct Hotel<T> {
     reservations: HashMap<String, u32>
 }
 
-impl<T> Hotel<T> { // because we dont need the display trait affected on the construct, we separete it from the summarize one
+impl<T> Hotel<T> { // because we dont need the display trait be affected on the construct, we separete it from the summarize one
     fn new(name: T) -> Self {
         Self {
             name,
@@ -33,13 +39,13 @@ impl<T: Display> Hotel<T> {
     }
 }
 
-impl<T> Accommadation for Hotel<T> {
+impl <T> Accommodation for Hotel<T> {
     fn book(&mut self, name: &str, nights: u32) {
         self.reservations.insert(name.to_string(), nights);
     }
 }
 
-impl<T> Description for Hotel<T> {} // in this way we're just using the default fn there.
+impl<T> Description for Hotel<T> {} // we're using the default definition
 
 #[derive(Debug)]
 struct AirBnb {
@@ -51,16 +57,17 @@ impl AirBnb {
     fn new(host: &str) -> Self {
         Self {
             host: host.to_string(),
-            guests: vec![]
+            guests: vec![],
         }
     }
 }
 
-impl Accommadation for AirBnb {
+impl Accommodation for AirBnb {
     fn book(&mut self, name: &str, nights: u32) {
         self.guests.push((name.to_string(), nights));
     }
 }
+
 impl Description for AirBnb {
     fn get_description(&self) -> String {
         format!("Please enjoy {}'s apartment", self.host)
@@ -69,7 +76,7 @@ impl Description for AirBnb {
 
 // multiple trait bound for generic trait bounds example
 // the entity must to implement both, not just one.
-fn book_for_one_night<T: Accommadation + Description>(entity: &mut T, guest: &str) {
+fn book_for_one_night<T: Accommodation + Description>(entity: &mut T, guest: &str) {
     entity.book(guest, 1);
 }
 
@@ -77,29 +84,35 @@ fn book_for_one_night<T: Accommadation + Description>(entity: &mut T, guest: &st
 fn mix_and_match<T, U>(
     first: &mut T,
     second: &mut U,
-    guest: &str
-) where
-    T: Accommadation + Description,
-    U: Accommadation
+    guest: &str,
+) where 
+    T: Accommodation + Description,
+    U: Accommodation
 {
     first.book(guest, 1);
     first.get_description();
 
-    second.book(guest, 3);
+    second.book(guest,3);
 }
 
-fn choose_best_place_to_stay() -> impl Accommadation + Description {
+fn choose_best_place_to_stay() -> impl Accommodation + Description {
     AirBnb::new("The Luxe")
 }
 
 fn main() {
     let hotel1 = Hotel::new(String::from("The Luxe")); // this implement the display trait
-    println!("{}", hotel1.summarize());
+    println!("{}", hotel1.summarize()); //
 
-    let hotel2 = Hotel::new("The Golden Standard");
-    println!("{}", hotel2.summarize()); // this implement display trait too
+    let hotel2 = Hotel::new(String::from("The Golden Standard")); // this implement the display trait too
+    println!("{}", hotel2.summarize());
 
     let hotel3 = Hotel::new(vec!["The Sweet Escape", "Hilton Edition"]);
-    // println!("{}", hotel3.summarize()); // this doesn't work, summarize exists but cannot be used
+
+    // Error: Missing trait bound and coherence rules
+    // this doesn't work, summarize exists but cannot be used for vec!, coz they not implement the display trait
+    // method cannot be called on `Hotel<Vec<&str>>` due to unsatisfied trait bounds
+    // println!("{}", hotel3.summarize());
 }
 
+// The compilation failure you're facing (for hotel3.summarize()) is related to trait bounds not being
+// satisfied. This is about coherence rules and method resolution.
