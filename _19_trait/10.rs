7bc3a3a70a60781464f6b9f705d920cd5f8f33b6
David <marcelobiagiola01@gmail.com>
2025-07-24 15:33:50 -0400

    example: some traits are need it to be called as a method

--- _19_trait/10.rs
+++ _19_trait/10.rs
@@ -2,13 +2,13 @@
 
 // lasted lectures we defined our traits in the same file but now
 // in our case we need to imported because its definition is somewhere else
-use std::ops::Add; // be brint the Add trait from the standard operation module
+use std::ops::Add; // be bring the Add trait from the standard operation module
 use std::str::FromStr;
 
 fn main() {
     let a: i32 = 5;
     let b: i32 = 10;
-    let sum = a.add(b);
+    let sum = a.add(b); // once imported, it's available 
     println!("{sum}");
 
     let numeric_count = u64::from_str("5");
