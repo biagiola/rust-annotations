a2831edcd962945a1a8d94f0473f3199a1ab9df0
David <marcelobiagiola01@gmail.com>
2025-07-26 18:43:06 -0400

    add key point on partial_cmp

--- _21_trait_2/17.rs
+++ _21_trait_2/17.rs
@@ -1,6 +1,7 @@
 // Lecture: Implementing the PartialOrd trait
 use std::cmp::Ordering;
 
+#[derive(Eq)]
 struct Job {
     salary: u32,
     commute_time: u32,
@@ -12,9 +13,14 @@ impl PartialEq for Job {
     }
 }
 
-impl Eq for Job {}
+// impl Eq for Job {} // no need this if we're using in the derive
 
-impl PartialOrd for job {
+impl PartialOrd for Job {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.salary.partial_cmp(&other.salary) // u32 implements PartialOrd
+    }
+
+    // same as above
     // fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
     //     if self.salary == other.salary {
     //         Some(Ordering::Equal)
@@ -26,10 +32,6 @@ impl PartialOrd for job {
     //         None
     //     }
     // }
-    // a short version
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.salary.partial_cmp(&other.salary) // coz u32 implements the partialOrd
-    }
 }
 
 fn main() {
@@ -48,14 +50,35 @@ fn main() {
     println!("{}", long_commute_job == short_commute_job);
     println!("{}", long_commute_job >= short_commute_job);
     println!("{}", long_commute_job <= short_commute_job);
-    // lt, le, gt, ge
 }
 
-// Rust Definition
-// pub trait PartialOrd<Rhs = Self>: PartialEq<Rhs>
-// where
-//     Rhs: ?Size,
-// {
-//     // Required method
-//     fn partial_cmp(&self, other: &Rhs) -> Option<Ordering>; // cmp means compare
+// Side notes.
+
+// PartialEq gives you == and !=
+// . Requires symmetry and transitivity.
+// . No promise that a == a is always true (floats break this with NaN).
+// . Eq is a marker “child” of PartialEq.
+// . Adds the promise of reflexivity (a == a).
+// . Many containers (HashMap, HashSet) require keys to be Eq.
+
+// Ordering Enum definition
+// pub enum Ordering {
+//     Less,    // self < other
+//     Equal,   // self == other
+//     Greater, // self > other
 // }
+
+// PartialOrd trait definition
+// pub trait PartialOrd: PartialEq {
+//     fn partial_cmp(&self, other: &Self) -> Option<Ordering>;
+
+//     // default methods automatically provided once you implement `partial_cmp`
+//     fn lt(&self, other: &Self) -> bool { … }
+//     fn le(&self, other: &Self) -> bool { … }
+//     fn gt(&self, other: &Self) -> bool { … }
+//     fn ge(&self, other: &Self) -> bool { … }
+// }
+
+// Key points:
+// partial_cmp returns Option<Ordering> → it might be None when the two values simply cannot be meaningfully ordered (again NaN for floats, or your own domain-specific reasons).
+// Once you implement partial_cmp ONCE, Rust automatically supplies all 6 comparison operators (<, <=, >, >=, and the method aliases above).
