b3b6fce0fdeb54623fca0f9bcffb2d23f92ff0ed
David <marcelobiagiola01@gmail.com>
2025-07-26 13:06:28 -0400

    default PartialEq trait compare all fields

--- _21_trait_2/13.rs
+++ _21_trait_2/13.rs
@@ -1,8 +1,10 @@
 // Lecture: implementing the partialEq trait on structs
 // this trait establishes equality between two values
 
-// #[derive(PartialEq)] // if we use this, instead of our implementation of partialEq
-// it will compare all the fields
+// PartialEq trait can be used on the Flight struct as usual, #[derive(PartialEq)], but
+// the default implementation will compare all the fields. In our case we're just comparing
+// origin and destination fields.
+
 struct Flight {
     origin: String,
     destination: String,
@@ -20,19 +22,20 @@ impl Flight {
 }
 
 impl PartialEq for Flight {
-    // self it's the flight and Self will be what we're going to compare
     fn eq(&self, other: &Self) -> bool {
-        // we're suppose, according to our bussiness logic that,
-        // with origin and destination, our structs are equals so...
         self.origin == other.origin && self.destination == other.destination
     }
 }
 
 fn main() {
-    let a = Flight::new("New York", "London", "11:00am");
-    let b = Flight::new("New York", "London", "12:00am");
-    let c = Flight::new("New York", "Los Angeles", "15:00am");
+    let a = Flight::new("New York", "London", "11:00AM");
+    let b = Flight::new("New York", "London", "12:00AM");
+    let c = Flight::new("Barcelona", "Los Angeles", "15:00AM");
+
     println!("{}", a == b);
-    println!("{}", a.eq(&b)); // also in this way, that's used behind the scenes
-    println!("{}", a.ne(&b)); // and its negation method as well
+    // same as above. this is used behind the scenes
+    println!("{}", a.eq(&b));
+
+    // this is the opposite
+    println!("{}", a.ne(&b));
 }
