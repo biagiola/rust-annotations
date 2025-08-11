94602dd8abd319e69f7affac6e8f6b7fcd6ca57e
David <marcelobiagiola01@gmail.com>
2025-08-10 21:14:35 -0400

    the filter method example

---
+++ _24_iterators/27.rs
@@ -0,0 +1,30 @@
+fn main() {
+    // The copied method converts an iterator
+    // from storing &T elements to T elements.
+    // It makes a copy of each T element.
+    // The T data type must implement the Copy trait.
+
+    // The cloned method similarly converts and
+    // iterator from storing &T elements to T elements.
+    // It makes a clone of each T element.
+    // The T data type must implement the Clone trait.
+    let teas = [
+        String::from("Hot Earl Grey"),
+        String::from("Iced Green"),
+        String::from("Hot Matcha"),
+    ];
+
+    // iterator has &String --> String
+    let more_teas: Vec<String> = teas.iter().cloned().collect();
+    println!("{more_teas:?}");
+    println!("{teas:?}");
+
+    // it's important to filter before cloned for optimazations
+    let more_teas: Vec<String> = teas
+        .iter()
+        .filter(|tea| tea.contains("Hot"))
+        .cloned()
+        .collect();
+    println!("{more_teas:?}");
+
+}
