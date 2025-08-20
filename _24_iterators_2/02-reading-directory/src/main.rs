b29c0cf5f2d47c6d3406c1875399edbbd2353cfa
David <marcelobiagiola01@gmail.com>
2025-08-20 10:11:42 -0400

    code refactor

--- _24_iterators_2/02-reading-directory/src/main.rs
+++ _24_iterators_2/02-reading-directory/src/main.rs
@@ -7,21 +7,50 @@ The fs::metadata function returns a Metadata struct.
 The Metadata struct includes an 'is_file' and 'is_directory' method.
 The fs::read_to_string function returns a io::Result<String>
 */
+// use std::fs;
+// use std::process;
+// fn main() {
+//     let directory = fs::read_dir("./").unwrap_or_else(|error| {
+//         eprintln!("Could not read directory: {error}");
+//         process::exit(1);
+//     });
+
+//     for entry_result in directory {
+//         match entry_result {
+//             Ok(entry) => println!("{:?}", entry.path()),
+//             Err(error) => {
+//                 eprintln!("Could not read entry: {error}");
+//             }
+//         }
+//     }
+// }
+
+// use std::fs;
+// use std::io;
+// fn main() -> Result<()> {
+//     for entry_result in fs::read_dir("./")? {
+//         if let Ok(entry) = entry_result {
+//             println!("{:?}", entry.path());
+//         }
+//     }
+
+//     Ok(())
+// }
+
 use std::fs;
-use std::process;
+use std::io;
 
-fn main() {
-    let directory = fs::read_dir("./").unwrap_or_else(|error| {
-        eprintln!("Could not read directory: {error}");
-        process::exit(1);
-    });
-
-    for entry_result in directory {
-        match entry_result {
-            Ok(entry) => println!("{:?}", entry.path()),
-            Err(error) => {
-                eprintln!("Could not read entry: {error}");
-            }
+fn main() -> io::Result<()> {
+    for entry_result in fs::read_dir("./")? {
+        let entry = entry_result?;
+        let entry_name = entry.path();
+        let metadata = fs::metadata(&entry_name);
+        if metadata?.is_file() {
+            println!("{entry_name:?} \n-------------");
+            let contents = fs::read_to_string(&entry_name);
+            println!("{contents:?}");
         }
     }
+
+    Ok(())
 }
