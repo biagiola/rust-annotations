a09e62c6de7f001cb89718f54715578a1049698d
David Biagiola <biagiola@Davids-MacBook-Pro.local>
2025-09-21 10:56:35 -0300

    fix enum

--- _10_enums/4.rs
+++ _10_enums/4.rs
@@ -2,25 +2,23 @@
 // A struct variant stores associated data in field rather than by position.
 // Each piece of data has an associated name.
 #[derive(Debug)]
-struct Credential {
+struct Credentials {
     username: String,
-    password: String
+    password: String,
 }
 
 #[derive(Debug)]
-struct PaymentMethodType {
+enum PaymentMethodType {
     CreditCard(String),
     DebitCard(String),
-    Paypal(Credentials)
+    Paypal(Credentials),
 }
 
 fn main() {
     let credentials: Credentials = Credentials {
         username: String::from("David Biagiola"),
-        password: String::from("password123")
-    }
-    let mut paypal: PaymentMethodType = PaymentMethodType::Paypal(
-        credentials
-    );
+        password: String::from("password123"),
+    };
+    let mut paypal: PaymentMethodType = PaymentMethodType::Paypal(credentials);
     println!("{:#?}", paypal);
 }
