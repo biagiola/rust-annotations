2ba37478c830ea8cea6a018f0f26daef4c27eee1
David Biagiola <marcelobiagiola01@gmail.com>
2025-09-26 17:30:32 -0300

    add logs

--- _22_lifetimes/1.rs
+++ _22_lifetimes/1.rs
@@ -1,34 +1,15 @@
-// Lecture: The lifetime of a value refers to how long it is valid within the code.
-// A value's lifetime is the time during which it exists at a particular memory address.
-// Lifetimes prevents dangling references by specifying how long a reference should be valid.
-
 fn main() {
     let a = 1;
-
     {
         let b = 2;
+        println!("a is valid here {}", a);
     }
+
+    println!("b is invalid here");
 
     let c = String::from("Winter");
     let d = c;
+
+    println!("d took ownership of c {}", d);
+    println!("so c does not exists anymore");
 }
-
-// lifetime and scope almost always are very tight to each other
-// but of course we can have a lifetime end before the end
-// of its scope, for example when we passing ownership.
-
-fn main() {
-    let a = 1;
-
-    {
-        let b = 2;
-    }
-
-    let c = String::from("Winter");
-    drop(c);
-}
-
-// also, we can say that the lifetime of c will end when we 
-// pass it as argument to a function.
-// Its scope is still the main function but the lifetime is
-// not to the end of it.
