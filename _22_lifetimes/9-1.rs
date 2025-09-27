b1fbc043f20f38709e013b5645e9cb16a9a734c3
David Biagiola <marcelobiagiola01@gmail.com>
2025-09-27 17:12:08 -0300

    delete repeated example

--- _22_lifetimes/9-1.rs
+++ _22_lifetimes/9-1.rs
@@ -5,11 +5,10 @@ fn data(collection: &Vec<i32>) -> &Vec<i32> {
 fn main() {
     // let cities: Vec = vec!["Longon", "Paris", "Barcelona"];
     let cities: Vec<i32> = vec![1, 2, 3];
-    
+
     let result = data(&cities);
     println!("{result:?}");
 }
-
 
 // option 2
 fn data(collection: &[i32]) -> &[i32] {
@@ -18,11 +17,10 @@ fn data(collection: &[i32]) -> &[i32] {
 fn main() {
     // let cities: Vec = vec!["Longon", "Paris", "Barcelona"];
     let cities: Vec<i32> = vec![1, 2, 3];
-    
+
     let result = data(&cities[..3]);
     println!("{result:?}");
 }
-
 
 // option 3
 fn data(collection: &[i32]) -> &[i32] {
@@ -31,26 +29,12 @@ fn data(collection: &[i32]) -> &[i32] {
 fn main() {
     // let cities: Vec = vec!["Longon", "Paris", "Barcelona"];
     let cities: Vec<i32> = vec![1, 2, 3];
-    
+
     let result = data(&cities);
     println!("{result:?}");
 }
 
-
-// option 4
-fn data(collection: &[i32]) -> &[i32] {
-    &collection[..3]
-}
-fn main() {
-    // let cities: Vec = vec!["Longon", "Paris", "Barcelona"];
-    let cities: Vec<i32> = vec![1, 2, 3];
-    
-    let result = data(&cities);
-    println!("{result:?}");
-}
-
-
-// we cannot return a reference from a owned variable of the function we're calling
+// we cannot return a reference from a owned variable. Owned by the function is being called
 // fn data() -> &[i32] {
 //     let collection = vec![100, 90, 80];
 //     &collection
